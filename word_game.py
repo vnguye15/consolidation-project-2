62a6613db4928373b6415e4957ae269b70bc07d6
Viet Nguyen <vietnguyen0425@gmail.com>
2024-04-23 15:57:50 -0400

    Loops for player 1 & 2 to guess letter

--- word_game.py
+++ word_game.py
@@ -15,3 +15,34 @@ guessing_word = random.choice(word_bank)
 # Tell player the length of the word 
 print(f"The length of the word is: {len(guessing_word)}")
 print(f"The theme is animal")
+
+
+#First commit here
+
+
+# keep looping until game is guessed right 
+# word_guess = False
+# while not word_guess:
+#     player_one_letter_guess = input("Guess the letter in the word bank: ")
+#     if player_one_letter_guess in guessing_word:
+#         occurences_in_guessing_word = guessing_word.count(player_one_letter_guess)
+#         print(f"The letter appears in the word {occurences_in_guessing_word}")
+#         player_one_guesses += 1
+#     elif (player_one_letter_guess not in guessing_word):
+#         player_one_guesses += 1
+#     if len(player_one_letter_guess) > 1:
+#         print(f"You guessed a word, please guess a letter!")
+
+
+# while not word_guess:
+#     player_two_letter_guess = input("Guess the letter in the word bank: ")
+#     if player_two_letter_guess in guessing_word:
+#         occurences_in_guessing_word = guessing_word.count(player_two_letter_guess)
+#         print(f"The letter appears in the word {occurences_in_guessing_word}")
+#         player_two_guesses += 1
+#     elif (player_two_letter_guess not in guessing_word):
+#         player_two_guesses += 1
+#     if len(player_two_letter_guess) > 1:
+#         print(f"You guessed a word, please guess a letter!")
+
+# Commit 2 

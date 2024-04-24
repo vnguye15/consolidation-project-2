84f0b5843f99885d2360a55b5cf916a8bfba6011
Viet Nguyen <vietnguyen0425@gmail.com>
2024-04-24 17:13:23 -0400

    Fixed Word Banks did not properly print

--- word_game.py
+++ word_game.py
@@ -15,9 +15,15 @@ def word_game():
     guessing_word = random.choice(word_bank)
 
     #Display list to hold for letters guessed for player 1 & 2
-    player_one_word_bank = []
-    player_two_word_bank = []
+    p1_correct_letter_bank = []
+    p1_incorrect_letter_bank = []
 
+    p2_correct_letter_bank = []
+    p2_incorrect_letter_bank = []
+
+    #Scores for player 1 & 2
+    p1_score = 0
+    p2_score = 0
 
     # print(guessing_word)
 
@@ -27,55 +33,46 @@ def word_game():
 
 
     # keep looping until game is guessed right 
-    p1_word_guess = False
-     # while word isn't guessed yet and player 1 has up to 3 guesses
-    while (not p1_word_guess):
-        print("Player one's turn now")
-        player_one_letter_guess = input("Guess the letter in the word bank: ")
-        if player_one_letter_guess in guessing_word:
+    word_in_bank = False
+     # while word isn't guessed yet 
+    while (not word_in_bank):
+        #whenever p1 guesses letter, increase p1 score
+        p1_letter_guess = input("Player one it is now your turn, guess the letter: ")
+        p1_score += 1
+        #if letter guessed is in the word
+        if (p1_letter_guess in guessing_word):
+                # show letter bank with letter guessed 
+                p1_correct_letter_bank.append(p1_letter_guess)
+                #then ask if they want to try guessing again <-- will lines 43-45 result in letter being asked again?
+                print(p1_correct_letter_bank)
+                ask_letter = input("Would you like to guess letter again? (y/n): ")
 
-            # Section here covers if letter is properly guessed in
-            occurences_in_guessing_word = guessing_word.count(player_one_letter_guess)
-            print(f"The letter appears in the word {occurences_in_guessing_word} times")
-            print(player_one_word_bank.append(player_one_letter_guess))
-            player_one_guesses += 1
+                
+                if (ask_letter == 'n'):
+                    #if player doesn't want to guess letter again, give them option to guess word
+                    ask_word = input ("Would you like to guess the word? (y/n): ")
+                #  if player chose yes
+                    if (ask_word == 'y'):
+                        p1_word_guess = input("Guess the word: ")
+                        #if correct word is guessed, set to win. (Set word_in_bank to true to end loop)
+                        if (p1_word_guess == guessing_word):
+                            word_in_bank = True
+                            print("Congrats you won")
+                        #otherwise increase their number of wrong guesses by 1
+                        else:
+                            print("you guessed wrong try again")
+                            player_one_wrong_word_guesses += 1
+        #if player one gets 3 word guesses wrong than game ends
+        if (player_one_wrong_word_guesses >= 3):
+            print("You ran out of guesses, game over!")
+            break
+
+        #Otherwise if player 1 did not guess correct letter, keep track of times letter is guessed & add score                
+        else:
+            p1_incorrect_letter_bank.append(p1_letter_guess)
+            print(p1_incorrect_letter_bank)
+            p1_score += 1
             
-            #ask if player would like to guess again
-            ask_question = input("Would you like to guess again? (y/n): ")
-        #Repeat the task   
-        if (ask_question == 'y'):
-            
-            
-        elif (player_one_letter_guess not in guessing_word):
-            player_one_guesses += 1        
-        if len(player_one_letter_guess) > 1:
-            print(f"You guessed a word, please guess a letter!")
-        if player_one_guesses == 3:
-            print("You ran out of guesses, game over")
-            p1_word_guess = True
-    
 
-
-
-    # p2_word_guess = False
-    # while not p2_word_guess:
-    #     # while word isn't guessed yet and player 1 has up to 3 guesses
-    #     print("Player two's turn now")
-    #     player_two_letter_guess = input("Guess the letter in the word bank: ") 
-    #     if player_two_letter_guess in guessing_word:
-    #         occurences_in_guessing_word = guessing_word.count(player_two_letter_guess)
-    #         print(f"The letter appears in the word {occurences_in_guessing_word} times")
-    #         player_two_guesses += 1
-    #         player_two_letter_guess += 1
-    #     elif (player_two_letter_guess not in guessing_word):
-    #         player_two_guesses += 1
-    #     if len(player_two_letter_guess) > 1:
-    #         print(f"You guessed a word, please guess a letter!")
-    #     if player_two_guesses == 3:
-    #         print("You ran out of guesses, game over")
-    #         p2_word_guess = True
-        
-
-    
     word_game()
-    # Commit 2 
+    

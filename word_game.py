d0d2a782a79a9bd4044e1f6a768aef6205fd6647
Viet Nguyen <vietnguyen0425@gmail.com>
2024-04-30 16:37:09 -0400

    some indentation changes with Scott

--- word_game.py
+++ word_game.py
@@ -41,38 +41,52 @@ def word_game():
         p1_score += 1
         #if letter guessed is in the word
         if (p1_letter_guess in guessing_word):
-                # show letter bank with letter guessed 
+            #if the letter guessed IS NOT already in the word bank then add it
+            if (p1_letter_guess not in p1_correct_letter_bank):
+                # show letter bank with letter guessed & add score
                 p1_correct_letter_bank.append(p1_letter_guess)
-                #then ask if they want to try guessing again <-- will lines 43-45 result in letter being asked again?
-                print(p1_correct_letter_bank)
-                ask_letter = input("Would you like to guess letter again? (y/n): ")
+                print(f"Correct Letter Bank: {p1_correct_letter_bank}")
+                p1_score += 1
+            #otherwise ignore the same guessed letter and just add score by 1     
+            else:
+                print(f"Correct Letter Bank: {p1_correct_letter_bank}")
+                p1_score += 1
+            #After each guess, ask if they want to guess letter again    
+            ask_letter = input("Would you like to guess letter again? (y/n): ")
 
-                
-                if (ask_letter == 'n'):
-                    #if player doesn't want to guess letter again, give them option to guess word
-                    ask_word = input ("Would you like to guess the word? (y/n): ")
-                #  if player chose yes
-                    if (ask_word == 'y'):
-                        p1_word_guess = input("Guess the word: ")
-                        #if correct word is guessed, set to win. (Set word_in_bank to true to end loop)
-                        if (p1_word_guess == guessing_word):
-                            word_in_bank = True
-                            print("Congrats you won")
-                        #otherwise increase their number of wrong guesses by 1
-                        else:
-                            print("you guessed wrong try again")
-                            player_one_wrong_word_guesses += 1
+
+        #if player doesn't want to guess letter again, give them option to guess word
+        if (ask_letter == 'n'):
+            ask_word = input ("Would you like to guess the word? (y/n): ")
+            # if player chose yes
+            if (ask_word == 'y'):
+                p1_word_guess = input("Guess the word: ")
+                #if correct word is guessed, set to win. (Set word_in_bank to true to end loop)
+                if (p1_word_guess == guessing_word):
+                    word_in_bank = True
+
+                    #Show p1 their score along with congrats
+                    print(f"*** Congrats! P1 won with score: {p1_score} ***")
+                #otherwise increase their number of wrong guesses by 1
+                else:
+                    print("you guessed wrong try again")
+                    player_one_wrong_word_guesses += 1
         #if player one gets 3 word guesses wrong than game ends
         if (player_one_wrong_word_guesses >= 3):
             print("You ran out of guesses, game over!")
             break
-
         #Otherwise if player 1 did not guess correct letter, keep track of times letter is guessed & add score                
-        else:
-            p1_incorrect_letter_bank.append(p1_letter_guess)
-            print(p1_incorrect_letter_bank)
-            p1_score += 1
+        elif (p1_letter_guess not in guessing_word):
+            #if the guessed letter is not in incorrect letter bank, add onto the letter bank
+            if (p1_letter_guess not in (p1_incorrect_letter_bank)):
+                p1_incorrect_letter_bank.append(p1_letter_guess)
+                print(f"Incorrect Letter Bank: {p1_incorrect_letter_bank}")
+                p1_score += 1
+            else: # otherwise its not so we just ignore it 
+                print(f"Incorrect Letter Bank: {p1_incorrect_letter_bank}")
+                p1_score += 1
+            ask_word = input ("Would you like to guess the word? (y/n): ")
             
 
-    word_game()
+word_game()
     
